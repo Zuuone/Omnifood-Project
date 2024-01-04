9c672c5e7182a937f0f74544074c8157de7e104f
geogitlessons <zg@gmail.com>
2024-01-04 19:59:07 +0400

    update image size, add favicon and meta descriptio, update script.js regarding sticky navigation, add sticky class in index.html and add sticky styles in style.css

--- js/script.js
+++ js/script.js
@@ -39,6 +39,28 @@ allLinks.forEach(function (link) {
   });
 });
 
+///////////////////////////////////////////////////////
+// sticky navigation
+
+const sectionHeroEl = document.querySelector(".section-hero");
+const obs = new IntersectionObserver(
+  function (entries) {
+    const ent = entries[0];
+    if (ent.isIntersecting === false) {
+      document.body.classList.add("sticky");
+    }
+    if (ent.isIntersecting === true) {
+      document.body.classList.remove("sticky");
+    }
+  },
+  {
+    root: null,
+    threshold: 0,
+    rootMargin: "-80px",
+  }
+);
+obs.observe(sectionHeroEl);
+
 // scroll  back to top
 
 ///////////////////////////////////////////////////////
